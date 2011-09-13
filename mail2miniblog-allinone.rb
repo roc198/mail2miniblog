672dca3faacff02db0ac03c186a6d04a44b2c3f6
newdongyuwei <newdongyuwei@localhost>
2011-09-13 07:48:07 +0000

    twitter

--- mail2miniblog-allinone.rb
+++ mail2miniblog-allinone.rb
@@ -38,7 +38,7 @@ class Mail2MiniBlog  <  Sinatra::Base
         "<div>邮件收发微博:<br>授权<a href='#{href}'>邮件发(SINA)微博</a></div>
         <div><a href='http://code.google.com/p/mail2miniblog' target='_blank'>本站源码</a></div>
         <hr><div><a href='http://ruby.session.im' target='_blank'>try Ruby on WebSocket</a></div>
-	<hr><div><a href='http://twitter.session.im' target='_blank'>mail2twitter(邮件发witter)</a></div>
+	<hr><div><a href='http://twitter.session.im' target='_blank'>mail2twitter(邮件发twitter)</a></div>
         <hr><div><a href='http://weibo.session.im' target='_blank'>分享微博订阅</a></div>"
     end
     
