6fdaed32802dfdadedddfe3d69993ce309b1106a
newdongyuwei <newdongyuwei@localhost>
2011-12-26 12:17:34 +0300

    new cron

--- util/cron.sh
+++ util/cron.sh
@@ -1,12 +1,27 @@
 #crontab -u root -e
 # m h  dom mon dow   command
-#每2分钟检查一次进程是否还活着，死了就重启。
-#*/2 * * * * /bin/sh /opt/hg/mail2miniblog/util/cron.sh > /opt/hg/mail2miniblog/cron.log 2>&1
+#*/1 * * * * /bin/sh /opt/hg/mail2miniblog/util/cron.sh > /opt/hg/mail2miniblog/cron.log 2>&1
 
-count=`ps -wef|grep mail2miniblog-allinone.rb |grep -v grep |wc -l`
+count=`ps -wef|grep session.im.rb |grep -v grep |wc -l`
 if [ "$count" -eq 1 ]; then
-  echo "The mail2miniblog-allinone.rb process already run";
+  echo "The session.im.rb process already run";
 else
-  echo "Start the mail2miniblog-allinone.rb process now:"
-  nohup /usr/local/bin/ruby /opt/hg/mail2miniblog/mail2miniblog-allinone.rb &
+  echo "Start the session.im.rb  process now:"
+  nohup /usr/local/bin/ruby /opt/hg/mail2miniblog/session.im.rb &
+fi
+
+count=`ps -wef|grep mail2weibo.rb |grep -v grep |wc -l`
+if [ "$count" -eq 1 ]; then
+  echo "The mail2weibo.rb process already run";
+else
+  echo "Start the mail2weibo.rb process now:"
+  nohup /usr/local/bin/ruby /opt/hg/mail2miniblog/mail2weibo.rb &
+fi
+
+count=`ps -wef|grep mail2twitter.rb |grep -v grep |wc -l`
+if [ "$count" -eq 1 ]; then
+  echo "The mail2twitter.rb process already run";
+else
+  echo "Start the mail2twitter.rb  process now:"
+  nohup /usr/local/bin/ruby /opt/hg/mail2miniblog/mail2twitter.rb &
 fi
